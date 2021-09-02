51b7069228b65a22e078b920ec31f4ae9bd09d90
uktamyb <67410434+uktamyb@users.noreply.github.com>
2021-09-02 14:50:31 +0500

    Changes I've done some changes

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-const currentTime = new Date(2021, 1, 1, 8);
-const realTime = currentTime.getHours();
-
-let greeting;
-
-const someStyle = {
-  color: ""
-};
-
-if (realTime < 12) {
-  greeting = "Good morning";
-  someStyle.color = "red";
-} else if (realTime < 18) {
-  greeting = "Good afternoon";
-  someStyle.color = "green";
-} else {
-  greeting = "Good evening";
-  someStyle.color = "blue";
-}
-
 ReactDOM.render(
   <div>
     <h1 style={someStyle}>{greeting}</h1>
   </div>,
   document.getElementById("root")
 );
+
+// const currentTime = new Date(2021, 1, 1, 8);
+// const realTime = currentTime.getHours();
+
+// let greeting;
+
+// const someStyle = {
+//   color: ""
+// };
+
+// if (realTime < 12) {
+//   greeting = "Good morning";
+//   someStyle.color = "red";
+// } else if (realTime < 18) {
+//   greeting = "Good afternoon";
+//   someStyle.color = "green";
+// } else {
+//   greeting = "Good evening";
+//   someStyle.color = "blue";
+// }
